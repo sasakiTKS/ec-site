a1cdc9d5d30c962bcdbdc14ddc8bc187f1796be3
EC2 Default User <ec2-user@ip-172-31-13-99.ap-northeast-1.compute.internal>
2022-11-24 14:06:34 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :genres,only: [:index,:create,:edit,:update]
+  end
 # 顧客用# URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
