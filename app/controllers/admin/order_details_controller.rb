9f7a97bb9d40cf1e4dd7523e61ca2242e2063f07
EC2 Default User <ec2-user@ip-172-31-13-99.ap-northeast-1.compute.internal>
2022-12-17 06:29:37 +0000

    complete

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -8,10 +8,14 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
-    @order = Order.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
 		if @order_detail.update(order_detail_params)
 		   flash[:success] = "制作ステータスを変更しました"
-		   if @order_detail.making_status == "製作完了"
+		   if @order_detail.making_status == "製作中"
+		   	@order.update(status: "製作中")
+		   end
+		   if @order_details.count == @order_details.where(making_status: 3).count
 		   	@order.update(status: "発送準備中")
 		   end
 		   redirect_to admin_order_path(@order)
